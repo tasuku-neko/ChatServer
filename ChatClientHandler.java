96799d1fc5b7d0300c22a7643c0132598296f1a6
apochi <apochi1993@gmail.com>
2015-07-18 23:25:44 +0900

    Update ChatClientHandler.java

--- ChatClientHandler.java
+++ ChatClientHandler.java
@@ -5,5 +5,14 @@ import java.util.*;
 
 public class ChatClientHandler extends Thread{
 
+  void open() throws IOException{
+	  InetAddress address = socket.getInetAddress();
+	  System.out.println(address);
+	
+    InputStream socketIn = socket.getInputStream();
+    OutputStream socketOut = socket.getOutputStream();
+    in = new BufferedReader(new InputStreamReader(socketIn)); 
+    out = new BufferedWriter(new OutputStreamWriter(socketOut));
+  }             
 }
 
